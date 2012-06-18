b7429856e1073842061108596bc8d88b928a81ca
Michael Hanson <michael.b.hanson@gmail.com>
2012-06-18 06:35:26 -0700

    clojure-tests

---
+++ README.md
@@ -0,0 +1,7 @@
+Clojure tests
+=============
+
+Here is the documentation of my knowledge of Clojure. Each time I learn
+something new, I write a new test for it; this is to cement my
+knowledge, to create a personal reference, and to safeguard what I know
+against potential changes in the Clojure API.
---
+++ tests.clj
@@ -0,0 +1,49 @@
+(ns tests.michael)
+(use 'clojure.test)
+(alias 'string 'clojure.string)
+
+
+(deftest arithmetic
+  (is (= true (number? 3)))
+
+  (is (= 7 (+ 3 4)))
+  (is (= 3 (- 7 4)))
+  (is (= -3 (- 4 7)))
+
+  (is (= 12 (* 3 4)))
+  (is (= 12.0 (* 3 4.0)))
+
+  (is (= 7/3 (/ 7 3)))
+  (is (= 2.5 (/ 5 2.0))))
+
+
+(deftest strings
+  (is (= true (string? "Hello.")))
+
+  (is (= "Hello, Michael." (str "Hello, " "Michael.")))
+  (is (= "HELLO!" (string/upper-case "Hello!"))))
+
+
+(deftest collections
+  (is (= true (vector? [3 4 7])))
+  (is (= true (list? '(3 4 7))))
+  (is (= true (map? {3 4, 7 8})))
+  (is (= true (set? #{3 4 7})))
+
+  (is (= [3 4 7 8 9] (conj [3 4 7] 8 9)))
+  (is (= '(9 8 3 4 7) (conj '(3 4 7) 8 9))))
+
+
+(deftest functions
+  (defn square [x] (* x x))
+  (def cube (fn [x] (* x x x)))
+
+  (is (= true (fn? square)))
+
+  (is (= 9 (square 3)))
+  (is (= 27 (cube 3))))
+
+  (is (= 9 ((fn [x] (* x x)) 3)))
+
+
+(run-tests)
